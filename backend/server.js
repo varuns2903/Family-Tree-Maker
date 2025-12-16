892e4874ca2aef1fc392501cb5d396253f0cf8f3
Varun S <04varuns@gmail.com>
2025-12-16 15:24:38 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,8 @@ connectDB();
 app.use(cors({
   origin: 'https://family-tree-maker-beige.vercel.app',
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 
